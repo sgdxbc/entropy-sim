df6ac48e503ca141eae794f2d28a7a7ab8bec41e
sgdxbc <guangda@u.nus.edu>
2024-08-29 22:10:04 +0800

    Add basic simulation loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::{
-    collections::{HashMap, HashSet},
+    cmp::Reverse,
+    collections::{BinaryHeap, HashMap, HashSet},
+    iter::repeat_with,
     time::{Duration, Instant},
 };
 
 use rand::{rngs::StdRng, Rng, SeedableRng};
-use rand_distr::{Distribution, WeightedIndex};
+use rand_distr::{Distribution, Exp, WeightedIndex};
 
 #[global_allocator]
 static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
@@ -12,27 +14,30 @@ static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 type DegreeDistr = WeightedIndex<f64>;
 
 fn main() {
-    let num_node = 1_000_000;
+    let num_node = 100_000;
+    let num_node_packet = 10;
     let r = 1.6;
+    let parameters = SystemParameters {
+        num_initial_node: num_node,
+        enter_rate: 0.1,
+        leave_rate: 0.1,
+    };
 
-    let k = (num_node as f32 / r) as usize;
+    let k = ((num_node * num_node_packet) as f32 / r) as usize;
     println!("k = {k}");
-    let degree_weights = p_degree(k);
-    let degree_distr = WeightedIndex::new(degree_weights).expect("valid weights");
-
+    // let degree_distr = WeightedIndex::new(p_degree(k)).expect("valid weights");
     let mut rng = StdRng::seed_from_u64(117418);
-    let mut decoder = Decoder::new(k);
+    // let mut decoder = Decoder::new(k);
+
+    let mut system = System::new(parameters);
+    system.init(&mut rng);
     let mut period = Period(Instant::now());
-    for i in 0.. {
-        let fragment = sample_fragment(k, &degree_distr, &mut rng);
-        // println!("sampled = {fragment:?}");
-        decoder.receive(fragment);
-        if decoder.recovered() {
-            println!("recovered with {} fragment(s)", i + 1);
-            break;
-        }
-        period.run(|| println!("received {} / {}", i + 1, decoder.received.len()))
+    while system.now < 10_000_000 {
+        system.step(&mut rng);
+        period.run(|| eprint!("{:80}\r", system.report()))
     }
+    eprint!("{:80}\r", system.report());
+    eprintln!()
 }
 
 struct Period(Instant);
@@ -135,5 +140,113 @@ impl Decoder {
                 }
             }
         }
+    }
+}
+
+#[derive(Debug)]
+struct SystemParameters {
+    num_initial_node: usize,
+    enter_rate: f64,
+    leave_rate: f64,
+}
+
+type Step = u32;
+type DataId = u32;
+type ChunkId = u32;
+type Packet = HashSet<ChunkId>; // need DataId as well?
+
+#[derive(Debug)]
+struct System {
+    parameters: SystemParameters,
+    now: Step,
+    events: BinaryHeap<Reverse<(Step, Event)>>,
+    nodes: Vec<Node>,
+
+    num_node_enter: u32,
+    num_node_leave: u32,
+}
+
+#[derive(Debug, Default)]
+struct Node {
+    packets: HashMap<DataId, Vec<Packet>>,
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Event {
+    NodeEnter,
+    NodeLeave,
+}
+
+impl System {
+    fn new(parameters: SystemParameters) -> Self {
+        Self {
+            parameters,
+            now: 0,
+            events: Default::default(),
+            nodes: Default::default(),
+
+            num_node_enter: 0,
+            num_node_leave: 0,
+        }
+    }
+
+    fn init(&mut self, rng: &mut impl Rng) {
+        self.nodes
+            .extend(repeat_with(Node::default).take(self.parameters.num_initial_node));
+        self.push_node_enter_event(rng);
+        self.push_node_leave_event(rng)
+    }
+
+    fn step(&mut self, rng: &mut impl Rng) {
+        let Reverse((at, event)) = self.events.pop().expect("events not exhausted");
+        self.now = at;
+        use Event::*;
+        match event {
+            NodeEnter => {
+                self.num_node_enter += 1;
+                // TODO
+                let node = Node {
+                    packets: Default::default(),
+                };
+                self.nodes.push(node);
+                self.push_node_enter_event(rng)
+            }
+            NodeLeave => {
+                self.num_node_leave += 1;
+                // TODO
+                self.nodes.swap_remove(rng.gen_range(0..self.nodes.len()));
+                self.push_node_leave_event(rng)
+            }
+        }
+    }
+
+    fn report(&self) -> String {
+        format!(
+            "[{:>8}] {} node(s) {:.2}% enter/step {:.2}% leave/step",
+            self.now,
+            self.nodes.len(),
+            self.num_node_enter as f32 / self.now as f32 * 100.,
+            self.num_node_leave as f32 / self.now as f32 * 100.,
+        )
+    }
+
+    fn push_event(&mut self, after: Step, event: Event) {
+        self.events.push(Reverse((self.now + after, event)))
+    }
+
+    fn push_node_enter_event(&mut self, rng: &mut impl Rng) {
+        let after = Exp::new(self.parameters.enter_rate)
+            .expect("valid parameter")
+            .sample(rng)
+            .ceil() as _;
+        self.push_event(after, Event::NodeEnter)
+    }
+
+    fn push_node_leave_event(&mut self, rng: &mut impl Rng) {
+        let after = Exp::new(self.parameters.leave_rate)
+            .expect("valid parameter")
+            .sample(rng)
+            .ceil() as _;
+        self.push_event(after, Event::NodeLeave)
     }
 }
